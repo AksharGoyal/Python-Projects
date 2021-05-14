41ba9acb40abb0ad4c7f004e2a052134a44e053a
Akshar Goyal <38995624+AksharGoyal@users.noreply.github.com>
2021-05-14 19:28:30 +0530

    turtle module file

---
+++ red_heart.py
@@ -0,0 +1,33 @@
+import turtle
+
+colours = ['red','purple','blue','green','orange','yellow']
+t = turtle.Pen()
+turtle.bgcolor('black')
+t.pencolor('red')
+t.width(2)
+t.left(90)
+startingX = t.pos()[0]
+startingY = t.pos()[1]
+
+for x in range(45):
+    t.forward(5)
+    t.left(5)
+
+tracker = 0
+while t.pos()[0] < startingX:
+    t.forward(2)
+    tracker += 1
+
+t.left(2 * (360 - t.heading()))
+
+while tracker > 0:
+    t.forward(2)
+    tracker -= 1
+
+for x in range(45):
+    t.left(5)
+    t.forward(5)
+
+
+
+    
