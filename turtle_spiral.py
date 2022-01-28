19861268b1fc0fa070e053db7750bea790230b1d
Akshar Goyal <38995624+AksharGoyal@users.noreply.github.com>
2022-01-28 17:40:10 +0530

    Edited the repo

--- turtle_spiral.py
+++
@@ -1,9 +0,0 @@
-import turtle
-colors = ['mistyrose', 'slategray', 'sienna', 'yellowgreen', 'powderblue', 'khaki']
-t = turtle.Pen()
-turtle.bgcolor('black')
-for x in range(180):
-    t.pencolor(colors[x % 6])
-    t.width(x/100 + 1)
-    t.forward(x)
-    t.left(59)
