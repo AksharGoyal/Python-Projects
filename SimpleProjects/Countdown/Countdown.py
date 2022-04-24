bf86211a4e79de5221c73c8000f0c569fcb38321
Akshar Goyal <38995624+AksharGoyal@users.noreply.github.com>
2022-04-24 17:32:08 -0400

    Update Countdown.py

--- SimpleProjects/Countdown/Countdown.py
+++ SimpleProjects/Countdown/Countdown.py
@@ -1,19 +1,18 @@
 # import the time module
 import time
 
-def countdown(h, m, s):
-    # Ask for a name for our timer
-    timer_name = input('Enter a label for your timer: ')
-    
+def countdown(h, m ,s, timer_name):
+        
     # if no name provided
     if(len(timer_name) == 0):
         timer_name = 'Countdown Over'
+
     # convert the time into a number
     count = (h*3600) + (m*60) + s
     
     try:
         # the timer starts
-        while count:
+        while count >= 0:
             h, m, s = count//3600, (count%3600)//60, count%60
             timer = '{:02d}:{:02d}:{:02d}'.format(h, m, s)
             print(timer, end="\r")  # erases the printed time
@@ -27,10 +26,15 @@ def countdown(h, m, s):
         print("Timer Interrupted! Exiting!")
         exit()
 
-# Ask for values for our timer
-hour, min, sec = 0, 0, 0
-hour = int(input("Hello! Enter the hour for the timer: "))
+    # Ask for a name for our timer
+timer_name = input('Hello! Enter a label for your timer: ')
+# hour, min, sec = 0, 0, 0
+hour = int(input("Enter the hour for the timer: "))
 minute = int(input("Enter the minute for the timer: "))
-second = int(input("Hello! Enter the second for the timer: "))
+second = int(input("Enter the second for the timer: "))
 
-countdown(hour, minute, second)
+if hour < 0 or minute < 0 or second < 0:
+    print("Inputs should be non-negative!")
+    exit()
+
+countdown(hour, minute, second, timer_name)
