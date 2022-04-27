e5e503ce3d0ab4fd21a48f2abc28477fb91409d0
Akshar Goyal <38995624+AksharGoyal@users.noreply.github.com>
2022-04-27 12:29:36 -0400

    Update URL2QRCode.py

--- SimpleProjects/QRCode/URL2QRCode.py
+++ SimpleProjects/QRCode/URL2QRCode.py
@@ -2,17 +2,19 @@ import qrcode # import the required module
 
 # Enter the url to be converted
 url = input("Enter URL to convert to QRCode: ")
+# out = r'C:\Users\claim\Desktop\Programming\PY\SimpleProj\QRCode\QRcode.png' # Can be anything
 
 #           Version 1               #
 # This version is very simple way of converting
 # img = qrcode.make(url)
-# img.save('C:/Users/claim/Desktop/Programming/PYTHON/QRcode.png')
+# img.save(out)
 
 #           Version 2               #
 #           This version helps      #
 #                   in styling      #
-qr = qrcode.QRCode(version = 1, box_size = 10, border = 5) # setup dimensions
+qr = qrcode.QRCode(version = 1, box_size = 5, border = 5) # setup dimensions
 qr.add_data(url)    # add the url
 qr.make(fit=True)   # fits the background
-img = qr.make_image(fill_color = 'blue', back_color = 'green')      # Image is produced
-img.save('C:/Users/claim/Desktop/Programming/PYTHON/QRcode.png')    # Image with QRCode is saved
+img = qr.make_image(fill_color = 'blue', back_color = 'yellow') # Image is produced of particular design
+img.save(out)    # Image with QRCode is saved
+print('QRCode is created!')
